a9b170c7de5b91041b9f6ce85afca517933ea249
hannehue <haped20@student.sdu.dk>
2024-06-03 12:53:59 +0200

    ijsjs

--- take_photo.sh
+++ take_photo.sh
@@ -10,27 +10,30 @@ FILE=~/project/pics/$DATE/$TIME
 
 rpicam-still -t 0.01 -o $FILE.jpg
 
-touch $FILE.json
+if [ -f ~/pics/$DATE2/$FILENAME ] ; then
+  touch $FILE.json
 
-METADATA=$(exiftool -json $FILE.jpg)
+  METADATA=$(exiftool -json $FILE.jpg)
 
-# Extract subject distance, exposure time, and ISO from metadata
-SubjectDistance=$(echo "$METADATA" | jq -r '.[0]."SubjectDistance"')
-ExposureTime=$(echo "$METADATA" | jq -r '.[0]."ExposureTime"')
-ISO=$(echo "$METADATA" | jq -r '.[0]."ISO"')
+  # Extract subject distance, exposure time, and ISO from metadata
+  SubjectDistance=$(echo "$METADATA" | jq -r '.[0]."SubjectDistance"')
+  ExposureTime=$(echo "$METADATA" | jq -r '.[0]."ExposureTime"')
+  ISO=$(echo "$METADATA" | jq -r '.[0]."ISO"')
 
-json_body='{
-  "File Name": "'$TIME.jpg'",
-  "Create Date": "'$(date +'%Y-%m-%d %H:%M:%S.%3N%:z')'",
-  "Create Seconds Epoch": '$(date +%s.%3N)',
-  "Trigger": "'$1'",
-  "Subject Distance": "'$SubjectDistance'",
-  "Exposure Time": "'$ExposureTime'",
-  "ISO": "'$ISO'"
-}'
+  json_body='{
+    "File Name": "'$TIME.jpg'",
+    "Create Date": "'$(date +'%Y-%m-%d %H:%M:%S.%3N%:z')'",
+    "Create Seconds Epoch": '$(date +%s.%3N)',
+    "Trigger": "'$1'",
+    "Subject Distance": "'$SubjectDistance'",
+    "Exposure Time": "'$ExposureTime'",
+    "ISO": "'$ISO'"
+  }'
 
-DATETIME=$(date +"%d/%m/%Y %H:%M:%S")
-echo "$DATETIME Took photo with flag: $1" >>/home/emli/project/pics/logs.txt
+  DATETIME=$(date +"%d/%m/%Y %H:%M:%S")
+  echo "$DATETIME Took photo with flag: $1" >> /home/emli/project/pics/logs.txt
 
-echo $json_body >$FILE.json
-echo "Photo $FILE.jpg taken with trigger $1"
+  echo $json_body > $FILE.json
+  echo "Photo $FILE.jpg taken with trigger $1"
+
+fi
